4925dfd00306c790edfbfe775d981db6d056fcca
sasongko kumoro <skumoro@lely.com>
2021-04-23 22:13:29 +0200

    cleaning all code

--- 3d_reconstruction/3d_reconstruction.py
+++ 3d_reconstruction/3d_reconstruction.py
@@ -28,6 +28,9 @@ class ThreeDPipeline:
         self.lrcheck = True  # Better handling for occlusions
         self.extended = False  # Closer-in minimum depth, disparity range is doubled
         self.subpixel = True  # Better accuracy for longer distance, fractional disparity 32-levels
+        self.extend_disparity_range = True  # Optionally, extend disparity range to better visualize it
+        self.apply_color_map = True  # Optionally apply color map
+        self.project_colorized_disparity = True   # Option 1: project colorized disparity
         self.median = dai.StereoDepthProperties.MedianFilter.KERNEL_7x7 #Options: MEDIAN_OFF, KERNEL_3x3, KERNEL_5x5, KERNEL_7x7
         self.right_intrinsic = [[860.0, 0.0, 640.0], [0.0, 860.0, 360.0], [0.0, 0.0, 1.0]]
         self.pcl_converter = None
@@ -37,9 +40,11 @@ class ThreeDPipeline:
                           "xout_disparity",
                           "xout_rectif_left",
                           "xout_rectif_right"]
-
+        self.skip_streams = ['left', 'right', 'depth'] # Skip some streams for now, to reduce CPU load
         self.pipeline = dai.Pipeline()
         self.streams = ['left', 'right']
+        self.image_height = 480 #720
+        self.image_width = 852 #1280
 
     def point_cloud(self):
         """
@@ -83,7 +88,10 @@ class ThreeDPipeline:
                 cams[i].setBoardSocket(dai.CameraBoardSocket.LEFT)
             else:
                 cams[i].setBoardSocket(dai.CameraBoardSocket.RIGHT)
-            cams[i].setResolution(dai.MonoCameraProperties.SensorResolution.THE_720_P)
+            if self.image_height == 480:
+                cams[i].setResolution(dai.MonoCameraProperties.SensorResolution.THE_720_P)
+            if self.image_height == 720:
+                cams[i].setResolution(dai.MonoCameraProperties.SensorResolution.THE_720_P)
         return cams
 
     def create_stereo_depth_pipeline(self):
@@ -123,35 +131,27 @@ class ThreeDPipeline:
             max_disp *= 32
             disp_type = np.uint16  # 5 bits fractional disparity
             disp_levels = 32
-
         data, w, h = image.getData(), image.getWidth(), image.getHeight()
-        # TODO check image frame type instead of name
         if name == 'rgb_preview':
             frame = np.array(data).reshape((3, h, w)).transpose(1, 2, 0).astype(np.uint8)
         elif name == 'rgb_video': # YUV NV12
             yuv = np.array(data).reshape((h * 3 // 2, w)).astype(np.uint8)
             frame = cv2.cvtColor(yuv, cv2.COLOR_YUV2BGR_NV12)
         elif name == 'depth':
-            # TODO: this contains FP16 with (lrcheck or extended or subpixel)
             frame = np.array(data).astype(np.uint8).view(np.uint16).reshape((h, w))
         elif name == 'disparity':
             disp = np.array(data).astype(np.uint8).view(disp_type).reshape((h, w))
-
             # Compute depth from disparity (32 levels)
             with np.errstate(divide='ignore'): # Should be safe to ignore div by zero here
                 depth = (disp_levels * baseline * focal / disp).astype(np.uint16)
-
-            if 1: # Optionally, extend disparity range to better visualize it
+            if self.extend_disparity_range:
                 frame = (disp * 255. / max_disp).astype(np.uint8)
-
-            if 1: # Optionally, apply a color map
+            if self.apply_color_map:
                 frame = cv2.applyColorMap(frame, cv2.COLORMAP_HOT)
-                #frame = cv2.applyColorMap(frame, cv2.COLORMAP_JET)
-
             if self.pcl_converter is not None:
-                if 0: # Option 1: project colorized disparity
+                if self.project_colorized_disparity:
                     frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-                    pcl_converter.rgbd_to_projection(depth, frame_rgb, True)
+                    self.pcl_converter.rgbd_to_projection(depth, frame_rgb, True)
                 else: # Option 2: project rectified right
                     self.pcl_converter.rgbd_to_projection(depth, last_rectif_right, False)
                 self.pcl_converter.visualize_pcd()
@@ -171,78 +171,21 @@ class ThreeDPipeline:
         cams["right"].out.link(cam_stereo.right)
         xouts = self.create_xouts()
         self.create_stereo_link(cam_stereo, xouts)
-
         if self.out_rectified:
             self.streams.extend(['rectified_left', 'rectified_right'])
         self.streams.extend(['disparity', 'depth'])
-
         print("Creating DepthAI device")
-
-        "------- Lanjut Besok ------"
-
         with dai.Device(self.pipeline) as device:
             print("Starting pipeline")
             device.startPipeline()
-
-            in_streams = []
-            if not self.source_camera:
-                # Reversed order trick:
-                # The sync stage on device side has a timeout between receiving left
-                # and right frames. In case a delay would occur on host between sending
-                # left and right, the timeout will get triggered.
-                # We make sure to send first the right frame, then left.
-                in_streams.extend(['in_right', 'in_left'])
-            in_q_list = []
-            inStreamsCameraID = []
-            for s in in_streams:
-                q = device.getInputQueue(s)
-                in_q_list.append(q)
-                inStreamsCameraID = [dai.CameraBoardSocket.RIGHT, dai.CameraBoardSocket.LEFT]
-
-            # Create a receive queue for each stream
-            q_list = []
-            for s in self.streams:
-                q = device.getOutputQueue(s, 8, blocking=False)
-                q_list.append(q)
-
-            # Need to set a timestamp for input frames, for the sync stage in Stereo node
-            timestamp_ms = 0
-            index = 0
             while True:
-                # Handle input streams, if any
-                if in_q_list:
-                    dataset_size = 2  # Number of image pairs
-                    frame_interval_ms = 33
-                    for i, q in enumerate(in_q_list):
-                        #name = q.getName()
-                        #path = 'dataset/' + str(index) + '/' + name + '.png'
-                        #data = cv2.imread(path, cv2.IMREAD_GRAYSCALE).reshape(720*1280)
-                        tstamp = datetime.timedelta(seconds=timestamp_ms // 1000,
-                                                    milliseconds=timestamp_ms % 1000)
-                        img = dai.ImgFrame()
-                        #img.setData(data)
-                        img.setTimestamp(tstamp)
-                        img.setInstanceNum(inStreamsCameraID[i])
-                        img.setType(dai.ImgFrame.Type.RAW8)
-                        img.setWidth(1280)
-                        img.setHeight(720)
-                        q.send(img)
-                        if timestamp_ms == 0:  # Send twice for first iteration
-                            q.send(img)
-                        #print("Sent frame: {:25s}".format(path), 'timestamp_ms:', timestamp_ms)
-                    timestamp_ms += frame_interval_ms
-                    index = (index + 1) % dataset_size
-                    if 1: # Optional delay between iterations, host driven pipeline
-                        sleep(frame_interval_ms / 1000)
-                # Handle output streams
-                for q in q_list:
-                    name = q.getName()
-                    image = q.get()
-                    #print("Received frame:", name)
-                    # Skip some streams for now, to reduce CPU load
-                    if name in ['left', 'right', 'depth']: continue
-                    frame = self.convert_to_cv2_frame(name, image)
-                    cv2.imshow(name, frame)
+                for stream in self.streams:
+                    queue = device.getOutputQueue(stream, 8, blocking=False)
+                    name = queue.getName()
+                    image = queue.get()
+                    if name not in self.skip_streams:
+                        frame = self.convert_to_cv2_frame(name, image)
+                        cv2.imshow(name, frame)
                 if cv2.waitKey(1) == ord('q'):
                     break
 
